a48c6303ad5cfcf8049c263afbd2e2b3031758bb
Diriczi Zsolt-Csaba <z.diriczi@yahoo.com>
2023-11-22 16:27:22 +0200

    Add counter to filtering

--- scripts/script.sh
+++ scripts/script.sh
@@ -6,7 +6,11 @@ else
     count=0
     while read line
     do
-        echo $line | grep -e "^[A-Z][A-Za-z0-9 ,\.\!\?]*" | 
-        grep -e "[\!\?\.]$" | grep -ve ",[ ]*si"
+        if echo $line | grep -e "^[A-Z][A-Za-z0-9 ,\.\!\?]*" | 
+                grep -e "[\!\?\.]$" | grep -ve ",[ ]*si"
+        then
+            ((++count))
+        fi
     done
+    echo "Count is $count"
 fi
